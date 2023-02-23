1024cfff172f73b9aed456352876416397451cbc
nathaniel <dj467032@gmail.com>
2023-02-23 03:24:52 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const cors = require('cors')
+
+const teamRouter = require('./routes/teamRouter');
+app.use(teamRouter)
+
+app.use (cors())
+app.use(express.json());
+
+//error route
+app.use((req, res)=>{
+    res.status(404).send("Route does not exit")
+});
+
+const startserver = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        app.listen(PORT, ()=>{
+            console.log(`server running on ${PORT}`);
+    })
+}catch
+    (error){
+        console.log(error);
+    };
+}
+
+startserver();
